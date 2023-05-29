73f545baaccf50a97541ea588dd5b8104f85927d
IgnacioMarelli <ignacio.marelli@hotmail.com>
2023-05-29 14:25:43 -0300

    agregado el /api

--- src/app.js
+++ src/app.js
@@ -24,12 +24,12 @@ app.use(express.json());
 app.use(cookieParser(config.cookie_secret));
 app.use(express.static(__dirname+'/public'));
 app.set('views', __dirname+'/views');
-app.use('/products', router, express.static(__dirname+'/public'));
-app.use('/carts', routerCart, express.static(__dirname+'/public'));
-app.use('/session', routerUser, express.static(__dirname+'/public'));
-app.use('/chat', routerChat);
+app.use('/api/products', router, express.static(__dirname+'/public'));
+app.use('/api/carts', routerCart, express.static(__dirname+'/public'));
+app.use('/api/session', routerUser, express.static(__dirname+'/public'));
+app.use('/api/chat', routerChat);
 
-app.use((error, req, res, next)=>{
+app.use((error, res)=>{
     console.error({error});
     res.status(500).json({error});
 })
