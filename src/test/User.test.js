de43733f4be3a2b31cd7075441c216a6574a652e
IgnacioMarelli <ignacio.marelli@hotmail.com>
2023-06-30 15:03:30 -0300

    Modificado el docs para que se pueda autorizar

---
+++ src/test/User.test.js
@@ -0,0 +1,15 @@
+import Assert from 'assert';
+
+const assert = Assert.strict;
+
+describe(
+    'Users dao',
+    function () {
+        before(function () {
+            console.log('before');
+        })
+        beforeEach
+    }
+)
+//function () y no ()=> por que hacemos uso del this.mocha
+//La expresión regular que controla esto es: /(?=[A-Za-z0-9@#$%/^.,{}&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/g
