9dd1217d5ba624386e490104a5a721217550b3a9
IgnacioMarelli <ignacio.marelli@hotmail.com>
2023-07-04 18:07:20 -0300

    error haciendo el test

--- src/test/User.test.js
+++ src/test/User.test.js
@@ -1,49 +1,26 @@
 import { expect } from 'chai';
 import supertest from 'supertest';
-describe('UsersController', () => {
-    const requester = supertest('http://localhost:8080/api')
-    describe('Test de endpoints de Users', ()=>{
-        beforeEach(async function () {
-            const connection = await mongoose.connect('mongodb://localhost:27017/test');
-            await mongoose.connection.db.collection('usuarios').deleteMany({});
-        })
-      it('should call the postRegister method of the service and redirect to /api/session/login', async () => {
-        const res =await requester
-          .post('/api/session/register')
-          .send({ first_name: 'test', last_name:'users',email:'hio@hohtml.cpm',age:98,role:'admin', password: 'testpass' })
+import userService from '../dao/Repository/user.repository.js';
+import mongoose from 'mongoose';
+import config from '../../data.js';
+describe('Router users', () => {
+  const requester = supertest('http://localhost:8080/api')
 
-            expect(res.body).to.have.property('message', 'User registered successfully');
-            expect(userService.postRegister).to.have.been.calledOnce;
-          });
+  describe('Test de endpoints de Users', ()=>{
+    beforeEach(async function () {
+      const connection = await mongoose.connect(config.MONGO_URL_TEST,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
       });
-      it('should call the postLogin method of the service and return the user', (done) => {
-        supertest(app)
-          .post('/api/session/login')
-          .send({ username: 'testuser', password: 'testpass' })
-          .expect(200)
-          .end((err, res) => {
-            if (err) {
-              return done(err);
-            }
-            expect(res.body).to.have.property('username', 'testuser');
-            expect(userService.postLogin).to.have.been.calledOnce;
-            done();
-          });
-      });
-      it('should call the updateUser method of the service and return success', (done) => {
-        supertest(app)
-          .put('/api/users/1')
-          .send({ username: 'updateduser' })
-          .expect(200)
-          .end((err, res) => {
-            if (err) {
-              return done(err);
-            }
-            expect(res.body).to.have.property('message', 'User updated successfully');
-            expect(userService.updateUser).to.have.been.calledOnce;
-            done();
-          });
-      });
+      await mongoose.connection.db.collection('usuarios').deleteMany({});
+    });
+    it('deberia llamar al register', async function() {
+      const res = await requester
+        .post('/session/register')
+        .send({ first_name: 'test', last_name:'users',email:'hio@hohtml.cpm',age:98,role:'admin', password: 'testpass' });
+      expect(res.status).to.equal(200);
+      expect(res.body).to.deep.equal({ message: 'User registered successfully' });
+      expect(await userService.findByEmail('hio@hohtml.cpm')).to.be.true;
     });
   });
-  
+})
